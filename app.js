b9fb1a4f3d97701ed03264071367c46ba0f5762b
Travis Swift <travis.arthur.swift@gmail.com>
2022-03-30 22:29:16 -0500

    input final

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+const profileDataArgs = process.argv.slice(2);
 
-console.log('Hello Node!');
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
